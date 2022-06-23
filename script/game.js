4dbaade61538a22625cc9f1984b72fb8356a7f4a
davidsmith931 <radoslaw.mikus@gmail.com>
2022-06-24 00:28:19 +0200

    Added won, draw, lost classes, restart, click to play, pointing system

--- script/game.js
+++ script/game.js
@@ -1,6 +1,24 @@
-const resetGame = () => {
+class Game {}
+
+const clearOptions = () => {
   playerChoices.forEach((choice) => {
     choice.classList.remove("won");
+    choice.classList.remove("draw");
     choice.classList.remove("lost");
+    choice.classList.remove("active");
   });
+  aiChoice.classList.remove("won");
+  aiChoice.classList.remove("draw");
+  aiChoice.classList.remove("lost");
 };
+
+const resetGame = () => {
+  aiPoint.textContent = 0;
+  playerPoint.textContent = 0;
+  playerPoints.forEach((item) => item.classList.remove("scored"));
+  aiPoints.forEach((item) => item.classList.remove("scored"));
+  playerChoices.forEach((item) => item.classList.remove("active"));
+  clearOptions();
+};
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", resetGame);
--- script/options.js
+++ script/options.js
@@ -1,4 +1,11 @@
 "use strict";
+const playerChoices = document.querySelectorAll(".playerOption");
+const playerPoints = document.querySelectorAll(".playerPoints span");
+const playerPoint = document.querySelector(".centerResult span:first-of-type");
+const aiChoice = document.querySelector(".aiOption");
+const aiPoints = document.querySelectorAll(".aiPoints span");
+const aiPoint = document.querySelector(".centerResult span:last-of-type");
+
 class Option {
   constructor(rock, paper, scissors, lizard, spock) {
     this.rock = rock;
@@ -10,11 +17,19 @@ class Option {
 
   haveYouWon(aiChoice) {
     this.aiChoice = this[aiChoice];
-    if (this.aiChoice === true) {
+    clearOptions();
+    if (this.aiChoice) {
+      playerPoints[parseInt(playerPoint.textContent)].classList.add("scored");
+      playerPoint.textContent = parseInt(playerPoint.textContent) + 1;
+      document.querySelector(".aiOption").classList.add("lost");
       return "You won!";
     } else if (this.aiChoice === null) {
+      document.querySelector(".aiOption").classList.add("draw");
       return "Draw!";
     } else {
+      aiPoints[parseInt(aiPoint.textContent)].classList.add("scored");
+      aiPoint.textContent = parseInt(aiPoint.textContent) + 1;
+      document.querySelector(".aiOption").classList.add("won");
       return "You lost!";
     }
   }
@@ -27,9 +42,6 @@ const lizard = new Option(false, true, false, null, true);
 const spock = new Option(true, false, true, false, null);
 
 const playerChoice = function () {
-  const playerChoices = document.querySelectorAll(".playerOption");
-  const aiChoice = document.querySelector(".aiOption");
-
   playerChoices.forEach((choice) => {
     choice.addEventListener("click", (e) => {
       // RESET ACTIVES
@@ -38,10 +50,23 @@ const playerChoice = function () {
       });
       // ADD ACTIVE
       choice.classList.add("active");
-      const daChoice = eval(e.target.textContent);
-      console.log(daChoice.haveYouWon(aiChoice.textContent));
     });
   });
 };
 
+const playGame = () => {
+  const startButton = document.querySelector(".start");
+  startButton.addEventListener("click", () => {
+    if (document.querySelector(".active")) {
+      const daChoice = eval(
+        document.querySelector(".active").getAttribute("name")
+      );
+      console.log(daChoice.haveYouWon(aiChoice.getAttribute("name")));
+    } else {
+      console.log("Wybierz coś");
+    }
+  });
+};
+
 playerChoice();
+playGame();
